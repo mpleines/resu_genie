0c51023f60943da6361245b5974f52ef1a63adf3
maik pleines <maik.pleines@gmail.com>
2024-11-19 20:48:21 +0100

    restyle stepper

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,30 +7,37 @@ const resumeSteps: Steps = [
   {
     label: 'Job Advertisement',
     path: '/job-advertisement',
+    description: 'Enter the job advertisement for your resume.',
   },
   {
     label: 'Personal Information',
     path: '/personal-information',
+    description: 'Enter your personal information.',
   },
   {
     label: 'Skills',
     path: '/skills',
+    description: 'Enter your skills.',
   },
   {
     label: 'Work Experience',
     path: '/work-experience',
+    description: 'Enter your work experience.',
   },
   {
     label: 'Education',
     path: '/education',
+    description: 'Enter your education.',
   },
   {
     label: 'Summary',
     path: '/summary',
+    description: 'A summary of your resume.',
   },
   {
     label: 'Download Resume',
     path: '/download-resume',
+    description: 'Download your resume.',
   },
 ];
 
@@ -39,11 +46,11 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
     <div className="min-h-screen flex flex-col">
       <Header />
       <StepperProvider steps={resumeSteps}>
-        <main className="py-16 px-16 flex-1 overflow-y-auto">
+        <main className="mx-auto max-w-screen-2xl w-full py-16 px-16 flex-1 overflow-y-auto grid grid-cols-[300px_1fr] gap-16">
           <div>
             <Stepper />
           </div>
-          {children}
+          <div className="mt-4">{children}</div>
         </main>
       </StepperProvider>
     </div>
--- app/components/Stepper.tsx
+++ app/components/Stepper.tsx
@@ -1,9 +1,9 @@
 'use client';
 import React, { FunctionComponent } from 'react';
 import { useStepper } from '../(dashboard)/useStepper';
-import { Button } from '@/components/ui/button';
-import { Circle, Dot } from 'lucide-react';
 import { usePathname } from 'next/navigation';
+import { Check, Circle, Dot } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 interface StepperProps {}
 
@@ -12,32 +12,40 @@ const Stepper: FunctionComponent<StepperProps> = () => {
   const currentPath = usePathname();
 
   return (
-    <div className="py-2 flex flex-wrap gap-4 justify-center items-center">
+    <div className="py-2 flex flex-col flex-wrap gap-4 justify-center items-center">
       {steps.map((step) => {
-        const index = steps.findIndex((s) => s.path === step.path);
         const isDisabled = comesAfterCurrentStep(step);
+        const isActive = currentPath === step.path;
+        const isCurrent = currentPath === step.path;
 
         return (
-          <React.Fragment>
-            <Button
-              variant="link"
-              disabled={isDisabled}
-              onClick={() => navigateTo(step)}
-            >
-              <Circle />
-              {step.label}
-            </Button>
-            {index < steps.length - 1 && (
-              <Dot
-                className={
-                  currentPath !== step.path &&
-                  steps.findIndex((s) => s.path === currentPath) < index
-                    ? 'opacity-50'
-                    : ''
-                }
-              />
-            )}
-          </React.Fragment>
+          <div
+            className="flex flex-col items-center gap-2 justify-center p-4 w-[250px] relative"
+            key={step.path}
+          >
+            <div className="w-full flex justify-center">
+              <button
+                className={cn(
+                  'rounded-full',
+                  isActive &&
+                    'ring-1 ring-offset-2 ring-primary ring-offset-background',
+                  'bg-black',
+                  'text-white p-1'
+                )}
+                disabled={isDisabled}
+                onClick={() => navigateTo(step)}
+              >
+                {isCurrent && <Circle />}
+                {!isCurrent && (!isDisabled ? <Check /> : <Dot />)}
+              </button>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="font-bold">{step.label}</span>
+              <span className="text-muted-foreground text-sm truncate">
+                {step?.description}
+              </span>
+            </div>
+          </div>
         );
       })}
     </div>
