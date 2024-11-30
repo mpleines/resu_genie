ac97eb93f744ceff749761dad8f3986db4f1a595
maik pleines <maik.pleines@gmail.com>
2024-11-30 14:12:52 +0100

    add ignoreDuringBuilds

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   // remove when ready for production, this is just for testing the deployment
   // !! WARN !!
   ignoreBuildErrors: true,
+  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
