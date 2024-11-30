fd8382640dcf3c4181904ea29c17e062c860af1f
maik pleines <maik.pleines@gmail.com>
2024-11-30 14:09:08 +0100

    add ignoreBuildErrors for testing deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // !! WARN !!
+  // remove when ready for production, this is just for testing the deployment
+  // !! WARN !!
+  ignoreBuildErrors: true,
+};
 
 export default nextConfig;
