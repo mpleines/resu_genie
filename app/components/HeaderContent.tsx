490714d844df2d08a302fbf9f0e03f552a67c34b
maik pleines <maik.pleines@gmail.com>
2025-02-24 17:14:46 +0100

    improve header layout on mobile

--- app/components/HeaderContent.tsx
+++ app/components/HeaderContent.tsx
@@ -1,39 +1,98 @@
+'use client';
+
 import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import { SignInButton } from './AuthButtons';
 import UserDropdown from './UserDropdown';
 import Image from 'next/image';
 import Logo from '@/public/logo.svg';
-import { auth } from '@/auth';
+import { Button } from '@/components/ui/button';
+import { MenuIcon } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
-const HeaderContent: FunctionComponent = async () => {
-  const session = await auth();
+const HeaderContent: FunctionComponent = () => {
+  const session = useSession();
 
   const renderSessionContent = () => {
-    if (session?.user != null) {
+    if (session.status === 'loading') {
+      return null;
+    }
+
+    if (session?.data?.user != null) {
       return (
         <div className="flex gap-6 items-center">
           <UserDropdown
-            username={session.user.name ?? 'Unknown User'}
-            avatarUrl={session.user.image ?? undefined}
+            username={session.data?.user.name ?? 'Unknown User'}
+            avatarUrl={session.data?.user.image ?? undefined}
           />
         </div>
       );
     }
 
     return (
-      <div className="flex items-center gap-4">
-        <Link
-          href="#how-it-works"
-          className="text-muted-foreground font-semibold"
-        >
-          How it works
-        </Link>
+      <div>
+        <Sheet>
+          <SheetTrigger>
+            <Button className="md:hidden">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-full">
+            <SheetHeader>
+              <SheetTitle>
+                <Link href="/" className="flex items-center gap-2">
+                  <Image width="30" height="30" alt="logo" src={Logo} />
+                  <h1 className="text-xl font-bold">resugenie.</h1>
+                </Link>
+              </SheetTitle>
+            </SheetHeader>
 
-        <Link href="#faq" className="text-muted-foreground font-semibold">
-          FAQ
-        </Link>
-        <SignInButton />
+            <div className="flex flex-col gap-4 mt-8">
+              <SheetClose className="flex" asChild>
+                <Link
+                  href="#how-it-works"
+                  className="text-muted-foreground font-semibold"
+                >
+                  How it works
+                </Link>
+              </SheetClose>
+
+              <SheetClose className="flex" asChild>
+                <Link
+                  href="#faq"
+                  className="text-muted-foreground font-semibold"
+                >
+                  FAQ
+                </Link>
+              </SheetClose>
+              <SheetClose className="flex" asChild>
+                <SignInButton />
+              </SheetClose>
+            </div>
+          </SheetContent>
+        </Sheet>
+
+        <div className="hidden md:flex items-center gap-4 ">
+          <Link
+            href="#how-it-works"
+            className="text-muted-foreground font-semibold"
+          >
+            How it works
+          </Link>
+
+          <Link href="#faq" className="text-muted-foreground font-semibold">
+            FAQ
+          </Link>
+          <SignInButton />
+        </div>
       </div>
     );
   };
