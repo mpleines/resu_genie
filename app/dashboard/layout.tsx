0df234f984ca4dfd925484cbaba5bbf998274a7b
maik pleines <maik.pleines@gmail.com>
2025-02-25 12:18:32 +0100

    fix layout shift on dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,9 +3,8 @@ export default function BasicLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // TODO: implement
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="flex flex-col">
       <main className="mx-auto max-w-screen-2xl w-full py-8 px-4 flex-1 overflow-y-auto">
         {children}
       </main>
