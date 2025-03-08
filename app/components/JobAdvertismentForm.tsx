3174570ecab0bb8036a9383ea8f09dd708c715f2
maik pleines <maik.pleines@gmail.com>
2025-03-08 13:29:23 +0100

    use multiple forms to handle validation of urls

--- app/components/JobAdvertismentForm.tsx
+++ app/components/JobAdvertismentForm.tsx
@@ -26,7 +26,7 @@ import StepperFooter from './StepperFooter';
 import { JobAdvertisement } from '@/types/types';
 import { Textarea } from '@/components/ui/textarea';
 import { Input } from '@/components/ui/input';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { FileTextIcon, LinkIcon, Loader2 } from 'lucide-react';
@@ -37,6 +37,12 @@ import AlertSuccess from './AlertSuccess';
 const formSchema = z.object({
   jobAdvertisement: z.string().min(1, { message: 'This field is required' }),
 });
+
+const crawlerFormSchema = z.object({
+  jobUrl: z.string().min(1).url(),
+});
+
+type InputMethod = 'url' | 'manual';
 
 type Props = {
   initialData: JobAdvertisement | null;
@@ -55,14 +61,22 @@ export default function JobAdvertisementForm({ initialData }: Props) {
   const [crawlStatus, setCrawlStatus] = useState<
     'not-started' | 'pending' | 'success' | 'failed'
   >('not-started');
-  const [crawlerUrl, setCrawlerUrl] = useState('');
 
-  const form = useForm<z.infer<typeof formSchema>>({
+  const [inputMethod, setInputMethod] = useState<InputMethod>('url');
+
+  const crawlerForm = useForm<z.infer<typeof crawlerFormSchema>>({
+    resolver: zodResolver(crawlerFormSchema),
+    defaultValues: {
+      jobUrl: '',
+    },
+  });
+  const submitForm = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       jobAdvertisement: initialData?.text ?? '',
     },
   });
+  const submitFormRef = useRef<HTMLFormElement>(null);
 
   async function submitJobAdvertisement(jobAd: string) {
     if (userId == null) {
@@ -102,8 +116,8 @@ export default function JobAdvertisementForm({ initialData }: Props) {
     await submitJobAdvertisement(jobAdvertisement);
   }
 
-  async function crawlJobAd() {
-    const url = encodeURIComponent(crawlerUrl);
+  async function crawlJobAd(values: z.infer<typeof crawlerFormSchema>) {
+    const url = encodeURIComponent(values.jobUrl);
     setCrawlStatus('pending');
     const response = await fetch(`/api/crawl-job/?url=${url}`);
     const json = await response.json();
@@ -114,7 +128,7 @@ export default function JobAdvertisementForm({ initialData }: Props) {
       return;
     }
 
-    form.reset({
+    submitForm.reset({
       jobAdvertisement: content,
     });
     setCrawlStatus('success');
@@ -122,126 +136,170 @@ export default function JobAdvertisementForm({ initialData }: Props) {
 
   function clear() {
     setCrawlStatus('not-started');
-    setCrawlerUrl('');
-    form.reset({ jobAdvertisement: '' });
+    crawlerForm.reset({ jobUrl: '' });
+    submitForm.reset({ jobAdvertisement: '' });
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <Card>
-          <CardHeader>
-            <CardTitle>Job Advertisement</CardTitle>
-            <CardDescription>
-              Paste a link to a job advertisement or enter the job details
-              manually
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Tabs defaultValue="url" className="w-full">
-              <TabsList className="grid w-full grid-cols-2 mb-6">
-                <TabsTrigger value="url" className="flex items-center gap-2">
-                  <LinkIcon className="h-4 w-4" />
-                  <span>Job URL</span>
-                </TabsTrigger>
-                <TabsTrigger value="text" className="flex items-center gap-2">
-                  <FileTextIcon className="h-4 w-4" />
-                  <span>Manual Entry</span>
-                </TabsTrigger>
-              </TabsList>
-              <TabsContent value="url" className="space-y-4">
-                <div className="space-y-2 flex flex-col">
-                  <Label htmlFor="job-url">Job Advertisement URL</Label>
-                  <Input
-                    id="job-url"
-                    placeholder="https://example.com/job-posting"
-                    value={crawlerUrl}
-                    onChange={(e) => setCrawlerUrl(e.target.value)}
-                    disabled={crawlStatus === 'success'}
-                  />
-                  <p className="text-sm text-muted-foreground">
-                    Paste a link to the job advertisement you want to create a
-                    resume for
-                  </p>
-                </div>
-
-                {crawlStatus !== 'success' && (
-                  <Button
-                    type="button"
-                    className="w-full md:w-1/4 self-end"
-                    onClick={crawlJobAd}
-                    disabled={crawlStatus === 'pending' || !crawlerUrl}
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Job Advertisement</CardTitle>
+          <CardDescription>
+            Paste a link to a job advertisement or enter the job details
+            manually
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Tabs
+            defaultValue="url"
+            className="w-full"
+            onValueChange={(value) => setInputMethod(value as InputMethod)}
+          >
+            <TabsList className="grid w-full grid-cols-2 mb-6">
+              <TabsTrigger value="url" className="flex items-center gap-2">
+                <LinkIcon className="h-4 w-4" />
+                <span>Job URL</span>
+              </TabsTrigger>
+              <TabsTrigger value="text" className="flex items-center gap-2">
+                <FileTextIcon className="h-4 w-4" />
+                <span>Manual Entry</span>
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="url" className="space-y-4">
+              <div className="space-y-2 flex flex-col">
+                <Label htmlFor="job-url">Job Advertisement URL</Label>
+                <Form {...crawlerForm}>
+                  <form
+                    onSubmit={crawlerForm.handleSubmit(crawlJobAd)}
+                    className="space-y-4"
                   >
-                    {crawlStatus === 'pending' && (
-                      <Loader2 className="animate-spin" />
-                    )}
-                    Submit URL
-                  </Button>
-                )}
+                    <div className="flex flex-wrap md:flex-nowrap items-center space-x-0 md:space-x-2 space-y-2 md:space-y-0">
+                      <FormField
+                        control={crawlerForm.control}
+                        name="jobUrl"
+                        render={({ field }) => (
+                          <FormItem className="w-full">
+                            <FormControl>
+                              <Input
+                                {...field}
+                                placeholder="https://example.com/job-posting"
+                                disabled={crawlStatus === 'success'}
+                                autoFocus
+                              />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
 
-                {crawlStatus === 'success' && (
-                  <AlertSuccess message="Job advertisement successfully fetched!" />
+                      <Button
+                        className="flex-1 md:flex-0"
+                        disabled={
+                          crawlStatus === 'pending' || crawlStatus === 'success'
+                        }
+                      >
+                        {crawlStatus === 'pending' && (
+                          <Loader2 className="animate-spin" />
+                        )}
+                        Submit URL
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+                {crawlerForm.formState.errors.jobUrl && (
+                  <span className="text-red-600 text-sm">
+                    {crawlerForm.formState.errors.jobUrl.message}
+                  </span>
                 )}
+                <p className="text-sm text-muted-foreground">
+                  Paste a link to the job advertisement you want to create a
+                  resume for
+                </p>
+              </div>
 
-                {crawlStatus === 'failed' && (
-                  <AlertDestructive message="Could not fetch job advertisement" />
-                )}
+              {crawlStatus === 'success' && (
+                <AlertSuccess message="Job advertisement successfully fetched!" />
+              )}
 
+              {crawlStatus === 'failed' && (
+                <AlertDestructive message="Could not fetch job advertisement" />
+              )}
+
+              {(crawlStatus === 'success' ||
+                submitForm.getValues().jobAdvertisement) && (
                 <div className="mt-4">
                   <div className="flex items-center justify-between flex-wrap py-2 ">
                     <h3 className="text-lg">Job Advertisement</h3>
                     <Button onClick={clear}>Clear & Try another</Button>
                   </div>
+                  <Form {...submitForm}>
+                    <form
+                      ref={submitFormRef}
+                      onSubmit={submitForm.handleSubmit(onSubmit)}
+                      className="space-y-4"
+                    >
+                      <FormField
+                        control={submitForm.control}
+                        name="jobAdvertisement"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <Textarea
+                                disabled
+                                {...field}
+                                className="bg-muted min-h-[250px]"
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </form>
+                  </Form>
+                </div>
+              )}
+            </TabsContent>
+            <TabsContent value="text" className="space-y-4">
+              <Form {...submitForm}>
+                <form
+                  ref={submitFormRef}
+                  onSubmit={submitForm.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
                   <FormField
-                    control={form.control}
+                    control={submitForm.control}
                     name="jobAdvertisement"
                     render={({ field }) => (
                       <FormItem>
                         <FormControl>
                           <Textarea
-                            disabled
+                            className="min-h-[250px]"
                             {...field}
-                            className="bg-muted min-h-[250px]"
+                            disabled={submitForm.formState.isSubmitting}
+                            placeholder="Paste in the job advertisement here manually"
                           />
                         </FormControl>
                         <FormMessage />
+
+                        <p className="text-sm text-muted-foreground">
+                          Enter the complete job description including
+                          requirements, responsibilities, and any other details
+                        </p>
                       </FormItem>
                     )}
                   />
-                </div>
-              </TabsContent>
-              <TabsContent value="text" className="space-y-4">
-                <FormField
-                  control={form.control}
-                  name="jobAdvertisement"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Textarea
-                          className="min-h-[250px]"
-                          {...field}
-                          disabled={form.formState.isSubmitting}
-                          placeholder="Paste in the job advertisement here manually"
-                        />
-                      </FormControl>
-                      <FormMessage />
-
-                      <p className="text-sm text-muted-foreground">
-                        Enter the complete job description including
-                        requirements, responsibilities, and any other details
-                      </p>
-                    </FormItem>
-                  )}
-                />
-              </TabsContent>
-            </Tabs>
-          </CardContent>
-        </Card>
-        <StepperFooter
-          showBackButton={false}
-          isSubmitting={form.formState.isSubmitting}
-        />
-      </form>
-    </Form>
+                </form>
+              </Form>
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+      </Card>
+      <StepperFooter
+        formRef={submitFormRef}
+        showBackButton={false}
+        isSubmitting={submitForm.formState.isSubmitting}
+        isDisabled={inputMethod === 'url' && !submitForm.formState.isValid}
+      />
+    </>
   );
 }
