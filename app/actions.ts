a3cbff7598274911bfe253c7ec3030dd7f0fd5d5
maik pleines <maik.pleines@gmail.com>
2025-06-16 14:00:14 +0200

    set resume job status to failed if error

--- app/actions.ts
+++ app/actions.ts
@@ -1,36 +1,36 @@
-"use server";
+'use server';
 
-import supabaseClient from "@/lib/supabase/server";
-import { Session } from "next-auth";
+import supabaseClient from '@/lib/supabase/server';
+import { Session } from 'next-auth';
 
 export async function generateResumeJob(
   resumeId: string,
-  user: Session["user"],
+  user: Session['user']
 ) {
   const supabase = supabaseClient();
 
   if (user?.email == null || user?.id == null) {
-    throw new Error("user email or id is null");
+    throw new Error('user email or id is null');
   }
 
   try {
     // Check if there's already a job for this resume
     const { data: existingJob } = await supabase
-      .from("resume_job")
+      .from('resume_job')
       .select()
-      .eq("resume_id", Number(resumeId))
+      .eq('resume_id', Number(resumeId))
       .single();
 
     // if the job is still pending or already done, return
-    if (existingJob?.status === "pending" || existingJob?.status === "done") {
+    if (existingJob?.status === 'pending' || existingJob?.status === 'done') {
       return { jobId: existingJob.id };
     }
 
     const { data: job, error } = await supabase
-      .from("resume_job")
+      .from('resume_job')
       .insert({
         created_at: new Date().toISOString(),
-        status: "pending",
+        status: 'pending',
         resume_id: Number(resumeId),
       })
       .select()
@@ -41,20 +41,26 @@ export async function generateResumeJob(
       return { jobId: null };
     }
 
-    const { error: edgeFnError } = await supabase
-      .functions.invoke("generate_resume", {
+    const { error: edgeFnError } = await supabase.functions.invoke(
+      'generate_resume',
+      {
         body: {
-          name: "Functions",
+          name: 'Functions',
           resumeJobId: job.id,
           resumeId,
           userId: user.id,
           userEmail: user.email,
         },
-      });
+      }
+    );
 
     if (edgeFnError) {
-      console.error("Error invoking generate_resume function:", edgeFnError);
-      throw new Error("Failed to invoke generate_resume function");
+      console.error('Error invoking generate_resume function:', edgeFnError);
+      await supabase
+        .from('resume_job')
+        .update({ status: 'failed' })
+        .eq('id', job.id);
+      throw new Error('Failed to invoke generate_resume function');
     }
 
     return { jobId: job.id };
